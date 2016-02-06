e9b9f951039d8e38d0456c7fd58bad84dc5a3518
martmwangi <luthermwangi2@gmail.com>
2016-02-06 18:06:24 +0300

    Setupe simple beaver slider

--- js/script.js
+++ js/script.js
@@ -7,3 +7,32 @@ $(function () {
     });
 })
 //For the slider
+$(function(){
+                var slider = new BeaverSlider({
+                    type: "zoomer",
+                    structure: {
+                        container: {
+                            id: "Gallery",
+                            width: 640,
+                            height: 500
+                        },
+                        controls: {
+                            containerClass: "control-container",
+                            elementClass: "control-element",
+                            elementActiveClass: "control-element-active",
+                            previewMode: true
+                        }
+                        
+                    },
+                    
+                    content: {
+                        images: ["Images/122-min.JPG","Images/142-min.JPG","Images/167-min.JPG"]
+                    },
+                    animation: {
+                        effects: effectSets["zoomer: photoShow"],
+                        interval: 3000,
+                        initialInterval: 2000,
+                        waitAllImages: true
+                    }
+                });
+            });
